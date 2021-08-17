5a1d6d9f5dff7b1766983f7fdb522eca8e1623d0
Sergio Pereda Saviñón <sergio@adtv.io>
2021-08-17 14:32:26 -0500

    meses faltantes

--- static/jessIndicadores.js
+++ static/jessIndicadores.js
@@ -20,5 +20,20 @@ export const jess = {
       { indicador: 'COVID19', esperado: 12, obtenido: 12 },
       { indicador: 'CMM Organizacional', esperado: 27, obtenido: 27 },
     ],
+    Mayo: [
+      { indicador: 'Bienestar integral', esperado: 20, obtenido: 20 },
+      { indicador: 'COVID19', esperado: 8, obtenido: 8 },
+      { indicador: 'CMM Organizacional', esperado: 22, obtenido: 22 },
+    ],
+    Junio: [
+      { indicador: 'Bienestar integral', esperado: 20, obtenido: 20 },
+      { indicador: 'COVID19', esperado: 13, obtenido: 12 },
+      { indicador: 'CMM Organizacional', esperado: 23, obtenido: 23 },
+    ],
+    Julio: [
+      { indicador: 'Bienestar integral', esperado: 20, obtenido: 20 },
+      { indicador: 'COVID19', esperado: 14, obtenido: 12 },
+      { indicador: 'CMM Organizacional', esperado: 30, obtenido: 30 },
+    ],
   },
 }
